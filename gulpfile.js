e884f8b6ff4c938496b9df72c2d91f12dacafcb6
Will Prescott <willprescott@Wills-MacBook-Pro.local>
2025-08-12 22:54:09 +0100

    feat: Migrate to ESLint 9 and fix linting issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,28 @@
-const { series, src, dest } = require('gulp');
-const fs = require('fs');
-const clean = require('gulp-clean');
-const eslint = require('gulp-eslint');
-const licenser = require('gulp-licenser');
-const requirejsOptimize = require('gulp-requirejs-optimize');
-const license = fs.readFileSync('./src/banner.txt', 'utf8');
+const { series, src, dest } = require("gulp");
+const fs = require("fs");
+const clean = require("gulp-clean");
+const eslint = require("gulp-eslint-new");
+const licenser = require("gulp-licenser");
+const requirejsOptimize = require("gulp-requirejs-optimize");
+const license = fs.readFileSync("./src/banner.txt", "utf8");
 
 function cleanOutput() {
-  return src(['dist/*', 'coverage/*'], {read: false})
-    .pipe(clean());
+  return src(["dist/*", "coverage/*"], { read: false }).pipe(clean());
 }
 
 function lint() {
-  return src('./src/**/*.js')
+  return src("./src/**/*.js")
     .pipe(eslint())
     .pipe(eslint.format())
-    .pipe(eslint.failAfterError())
+    .pipe(eslint.failAfterError());
 }
 
 function scripts() {
-  return src('./src/cdg.js', { sourcemaps: true })
+  return src("./src/cdg.js", { sourcemaps: true })
     .pipe(sourcemaps.init())
     .pipe(requirejsOptimize())
     .pipe(licenser(license))
-    .pipe(dest('dist', { sourcemaps: '.' }))
+    .pipe(dest("dist", { sourcemaps: "." }));
 }
 
 exports.cleanOutput = series(cleanOutput);
